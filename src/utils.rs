c7b6719abbd1492c85a127fdff834f34d23330dc
Antoine <aaltorffer@orange.fr>
2025-08-03 18:52:26 +0200

    Restore error to keep compatibilty with main crate.

--- src/utils.rs
+++ src/utils.rs
@@ -67,6 +67,8 @@ pub enum HassleError {
     },
     #[error("LibLoading error: {0:?}")]
     LibLoadingError(#[from] libloading::Error),
+    #[error("Windows only")]
+    WindowsOnly(String),
 }
 
 pub type Result<T, E = HassleError> = std::result::Result<T, E>;
